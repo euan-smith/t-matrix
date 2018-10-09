cc54ff1a4e31dfd751e2c874ac9d39298b324114
Euan Smith <euan1971@yahoo.co.uk>
2018-10-09 16:17:04 +0100

    adding a rollup build step

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+// rollup.config.js
+import nodeResolve  from 'rollup-plugin-node-resolve';
+import commonjs     from 'rollup-plugin-commonjs';
+import babel        from 'rollup-plugin-babel';
+import json         from 'rollup-plugin-json';
+
+export default {
+  input: 'index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'cjs',
+    sourcemap: true
+  },
+  plugins: [
+    nodeResolve({ jsnext: true }), // load npm modules from npm_modules
+    json(), // avoid the package.json parsing issue
+    commonjs(), // convert CommonJS modules to ES6
+    babel(), // convert to ES5
+  ]
+};
+
