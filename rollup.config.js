2bc68b1250ab8ad3e8cfe4b288701caad25c4522
Euan Smith <euan1971@yahoo.co.uk>
2018-10-09 17:29:23 +0100

    got es6 and cjs bundles

--- rollup.config.js
+++ rollup.config.js
@@ -6,11 +6,18 @@ import json         from 'rollup-plugin-json';
 
 export default {
   input: 'index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs',
-    sourcemap: true
-  },
+  output: [
+    {
+      file: 'dist/bundle.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/bundle.mjs',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
   plugins: [
     nodeResolve({ jsnext: true }), // load npm modules from npm_modules
     json(), // avoid the package.json parsing issue
